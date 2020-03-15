4baff2669dee73e1b1cf70d97ae90f7a39e9efc1
JarekIT <Keraj321>
2020-03-15 16:21:17 +0100

    #3 create Invoice model

---
+++ src/main/java/pl/jarekit/rael/Invoice.java
@@ -0,0 +1,87 @@
+package pl.jarekit.rael;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Table
+public @Data class Invoice {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="id_client_seller")
+    Client clientSeller;
+
+    @ManyToOne
+    @JoinColumn(name="id_client_buyer")
+    Client clientBuyer;
+
+    @Column
+    private String type;
+
+    @Column(name="invoice_number")
+    private String invoiceNumber;
+
+    @Column(name="date_sale")
+    private LocalDate dateSale;
+
+    @Column(name="date_create")
+    private LocalDate dateCreate;
+
+    @Column
+    private BigDecimal amount;
+
+    @Column
+    private String description;
+
+    @Column
+    private LocalDate period;
+
+    @Column
+    private int category;
+
+    @Column
+    private String comment;
+
+    @Transient
+    private BigDecimal[] amountType = new BigDecimal[4];
+
+    @Transient
+    private Client clientPartner;
+
+    public Invoice() {
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "id=" + id +
+                ", clientSeller=" + clientSeller +
+                ", clientBuyer=" + clientBuyer +
+                ", type='" + type + '\'' +
+                ", invoiceNumber='" + invoiceNumber + '\'' +
+                ", dateSale=" + dateSale +
+                ", dateCreate=" + dateCreate +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", period=" + period +
+                ", category=" + category +
+                ", comment='" + comment + '\'' +
+                '}' +
+                "\n" ;
+    }
+
+}
+
+
+
