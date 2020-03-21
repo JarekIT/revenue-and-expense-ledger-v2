9ed989fda0a1688d538e7a8ca3891b2714d256c0
JarekIT <Keraj321>
2020-03-21 14:42:45 +0100

    #38 create AppController

---
+++ src/main/java/pl/jarekit/rael/controller/AppController.java
@@ -0,0 +1,13 @@
+package pl.jarekit.rael.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String viewHomePage(){
+        return "index";
+    }
+}
