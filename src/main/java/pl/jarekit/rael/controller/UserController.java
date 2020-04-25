4be99eb4d9c3852afa594fcaaef66542a7871160
JarekIT <JaroslawDanielewiczIT@gmail.com>
2020-04-25 17:56:02 +0200

    #110 correct the typos

--- src/main/java/pl/jarekit/rael/controller/UserController.java
+++ src/main/java/pl/jarekit/rael/controller/UserController.java
@@ -1,19 +1,12 @@
 package pl.jarekit.rael.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import pl.jarekit.rael.model.User;
 import pl.jarekit.rael.service.UserService;
-
-import java.security.Principal;
-import java.util.Collection;
 
 @Controller
 public class UserController {
@@ -25,24 +18,6 @@ public class UserController {
         this.userService = userService;
     }
 
-
-    @GetMapping("/home")
-    public String get(Principal principal, Model model){
-        model.addAttribute("name",principal.getName());
-
-        Collection<? extends GrantedAuthority> role = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-        model.addAttribute("role",role);
-
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        model.addAttribute("id",user.getId());
-        model.addAttribute("password",user.getPassword());
-        model.addAttribute("client",user.getClientUser());
-
-        return "home";
-    }
-
-
-
     @GetMapping("/setup/{clientId}")
     public String setClientToLoggedUser(@PathVariable long clientId){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -50,7 +25,7 @@ public class UserController {
         return "redirect:/home";
     }
 
-    @RequestMapping("/changePassword/{newPassword}")
+    @GetMapping("/changePassword/{newPassword}")
     public String changePassword(@PathVariable String newPassword){
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         userService.setPasswordInUser(user, newPassword);
